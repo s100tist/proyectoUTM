4607fcbed7ad711077e6af51b137ae38e0fb028a
Rugal <scaryskriller@gmail.com>
2022-03-09 21:54:32 -0600

    Subida

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule} from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfesorComponent } from './components/profesor/profesor.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { GeneralesComponent } from './components/generales/generales.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
+import { ArticulosViceComponent } from './components/articulos-vice/articulos-vice.component';
+import { RecuperarComponent } from './components/recuperar/recuperar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProfesorComponent,
+    NavigationComponent,
+    GeneralesComponent,
+    HomeComponent,
+    ArticulosComponent,
+    ArticulosViceComponent,
+    RecuperarComponent,
+  ],
+  imports: [
+    BrowserModule,
+	FormsModule,
+	HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/recuperar/recuperar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CorreoService } from 'src/app/services/correo.service';
+
+@Component({
+  selector: 'app-recuperar',
+  templateUrl: './recuperar.component.html',
+  styleUrls: ['./recuperar.component.css']
+})
+export class RecuperarComponent implements OnInit {
+  token: any;
+    
+
+  constructor(private route : ActivatedRoute, private correoService : CorreoService) {
+    this.token = ''
+  }
+
+  ngOnInit(): void {
+      this.route.paramMap.subscribe(params =>
+        {
+            this.token = params.get("tokens");
+            let dato={
+              'token':this.token
+            }
+            console.log(this.token);
+            console.log('correo: '+this.correoService.decodificarMail(this.token).subscribe())
+        });
+  }
+  verificarContrasena(): void {
+}
+}
